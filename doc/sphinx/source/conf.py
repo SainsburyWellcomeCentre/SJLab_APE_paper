205f89ecd637742adf70c3955058ea2ee3ab95ed
HernandoMV <hernybiotec@gmail.com>
2022-12-13 14:37:25 +0000

    .

--- doc/sphinx/source/conf.py
+++ doc/sphinx/source/conf.py
@@ -13,12 +13,10 @@ author = 'Hernando M Vergara'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc']
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = []
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
