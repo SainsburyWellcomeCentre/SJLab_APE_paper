81f2af88d8ded61f0999c01a296fe9bb7b91dd02
YvonneJohansson <yvonnemaria.johansson@gmail.com>
2024-03-27 09:50:30 +0000

    Added plots of SFig5PQR

--- scripts/yj_plotting.py
+++ scripts/yj_plotting.py
@@ -48,3 +48,54 @@ def plot_SF5TU(APE_mean_trace, RTC_mean_trace, APE_sem_trace, RTC_sem_trace, APE
     fig.tight_layout(pad=2)
 
     return fig
+
+
+def plot_SF5PQR(all_group_data, curr_data_time, SOR_choice_peak_values, SOR_cue_peak_values):
+    fig, ax = plt.subplots(1, 3, figsize=(9, 3))
+    fig.tight_layout(pad=4)
+    x_range = [-2, 3]
+    y_range = [-1, 2]
+    nr_mice = len(all_group_data['SOR_cue'])
+    a = 0
+    align_to = ['SOR_cue', 'SOR_choice']
+    color_mean = ['#e377c2','#3F888F']
+    color_sem = ['#e377c2','#7FB5B5']
+    alphas = [0.4, 0.6]
+    labels = ['Sound', 'Choice']
+    # plotting the z-scored dLight traces
+    for a, key in enumerate(all_group_data.keys()):
+        curr_data = all_group_data[key]
+        curr_data_set_mean = np.mean(curr_data, axis=0)
+        curr_data_set_sem = np.std(curr_data, axis=0) / np.sqrt(nr_mice)
+
+        ax[a].plot(curr_data_time, curr_data_set_mean, lw=2, color=color_mean[a], label=labels[a])
+        ax[a].fill_between(curr_data_time, curr_data_set_mean - curr_data_set_sem,
+                               curr_data_set_mean + curr_data_set_sem, color=color_sem[a],
+                               linewidth=1, alpha=alphas[a])
+        ax[a].spines['top'].set_visible(False)
+        ax[a].spines['right'].set_visible(False)
+        ax[a].axvline(0, color='#808080', linewidth=0.5, ls='dashed')
+        ax[a].set_xlabel('Time (s)')
+        ax[a].set_ylabel('dLight z-score')
+        ax[a].set_xlim(x_range)
+        ax[a].set_ylim(y_range)
+        ax[a].legend(loc='upper right', frameon=False)
+
+    # plotting the peak values
+    for i in range(0, len(SOR_choice_peak_values)):
+        x_val = [0, 1]
+        y_val = [SOR_choice_peak_values[i], SOR_cue_peak_values[i]]
+        ax[2].plot(x_val, y_val, color='grey', linewidth=0.5)
+
+    zeroes = [0, 0, 0, 0, 0, 0]
+    ones = [1, 1, 1, 1, 1, 1]
+    ax[2].scatter(zeroes, SOR_choice_peak_values, color='#3F888F', s=100, alpha=1)
+    ax[2].scatter(ones, SOR_cue_peak_values, color='#e377c2', s=100, alpha=1)
+
+    ax[2].spines['top'].set_visible(False)
+    ax[2].spines['right'].set_visible(False)
+    ax[2].set_xticks([0, 1])
+    ax[2].set_xticklabels(['Choice', 'Sound'])
+    ax[2].set_ylabel('dLight z-score')
+    ax[2].set_xlim(-0.5, 1.5)
+    ax[2].set_ylim(y_range)
